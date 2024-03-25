51209041c4a04b23c259c34d6cf61ff832f93934
Kramarenko Maxim <kramarenko-90@inbox.ru>
2024-03-25 19:17:54 +0300

    charList fixing

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,46 +1,83 @@
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
 
+
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
-const CharList = () => {
-    const marvelService = new MarvelService();
+class CharList extends Component {
+    
+    state = {
+        charList: []
+    }
+    
+    marvelService = new MarvelService();
 
-    onMountList = (array) => {
+    componentDidMount() {
+        this.marvelService.getAllCharacters()
+        .then(this.onCharListLoaded)
+    }
+
+    onCharListLoaded = (charList) => {
+        this.setState({charList})
+    }
+
+    renderItems = (array) => {
         const newArr = array.map(item => {
             const {id, ...itemOptions} = item;
             return (
-                <charItem key={id} {...itemOptions}/>
+                <CharItem key={id} {...itemOptions}/>
             )
         })
 
         return newArr;
     }
+    
+    render() {
+        const {charList} = this.state;
+        const items = this.renderItems(charList)
 
-    updateListChars = () => {
-        this.marvelService
-        .getAllCharacters()
-        .then(onMountList)
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {items}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
     }
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-            updateListChars()
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+    }
 
-const charItem = ({name, thumbnail}) => {
-    return (
-        <li className="char__item">
-                    <img src={thumbnail} alt="abyss"/>
-                    <div className="char__name">{name}</div>
-        </li>
-    )
+class CharItem extends Component {
+    constructor({name, thumbnail}){
+        super({name, thumbnail});
+        this.state = {
+            name: name,
+            loading: true,
+            thumbnail: thumbnail
+        }
+    }
+    render() {
+        const {name, thumbnail, loading} = this.state;
+        
+        let imgStyle = {'objectFit': 'cover'}
+        if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            imgStyle = {'objectFit': 'contain'}
+        }
+
+        const li =
+            <li className="char__item">
+                <img src={thumbnail} alt="abyss" style={imgStyle}/>
+                <div className="char__name">{name}</div>
+            </li>
+        const spinner = loading ? <Spinner/> : null;    
+        const content = !(name || thumbnail) ? spinner : li;
+        
+        console.log(thumbnail);
+        return content;
+    }
 }
 
 
